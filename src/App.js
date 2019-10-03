096111add9cb3ebb0c30691bcf1fc8313dc3c933
Henry Munoz <alexcol123@yahoo.com>
2019-10-03 14:48:19 -0400

    create reduz actions and reducers and   brought provider and store to  App.js

--- src/App.js
+++ src/App.js
@@ -12,22 +12,27 @@ import AddContact from './components/contacts/AddContact';
 import About from './components/pages/About';
 import NotFound from './components/pages/NotFound';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App ">
-          <Header />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Contacts} />
-              <Route exact path="/contact/add" component={AddContact} />
-              <Route exact path="/about" component={About} />
-              <Route exact path="/notFound" component={NotFound} />
-            </Switch>
+      <Provider store={store}>
+        <Router>
+          <div className="App ">
+            <Header />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/contact/add" component={AddContact} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/notFound" component={NotFound} />
+              </Switch>
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
---
+++ src/reducers/contactReducer.js
@@ -0,0 +1,31 @@
+import { GET_CONTACTS, ADD_CONTACT, DELETE_CONTACT } from '../actions/types';
+
+const initialState = {
+  contacts: [
+    {
+      id: 1,
+      name: 'John Doe',
+      email: 'john@gmail.com',
+      phone: '555-555-5555'
+    },
+    {
+      id: 2,
+      name: 'Karen Williams',
+      email: 'karen@gmail.com',
+      phone: '444-444-4444'
+    },
+    {
+      id: 3,
+      name: 'Henry Johnson',
+      email: 'henry@gmail.com',
+      phone: '333-333-333'
+    }
+  ]
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
