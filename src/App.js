a41fcc63b9ea3377b819d29c5fe16f1dd4369b84
Henry Munoz <alexcol123@yahoo.com>
2019-10-03 10:28:36 -0400

    Initial folder setup

--- src/App.js
+++ src/App.js
@@ -2,10 +2,28 @@ import React, { Component } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
+
+import Header from './components/layout/Header';
 
 class App extends Component {
   render() {
-    return <div>Hello</div>;
+    return (
+      <Router>
+        <div className="App ">
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route exact path="/about" component={About} />
+              <Route exact path="/notFound" component={NotFound} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    );
   }
 }
 
